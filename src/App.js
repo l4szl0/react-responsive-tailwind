fad38dda79b8d46c5377e77a7d79fb1d06d45f93
l4szl0 <t0r0k.l4szl0@gmail.com>
2022-12-21 22:38:47 +0100

    add Offers component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero'
 import Navbar from './components/Navbar';
+import Offers from './components/Offers'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Offers />
     </div>
   );
 }
