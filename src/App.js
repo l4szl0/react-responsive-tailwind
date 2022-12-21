9a2d8be684451e4731212ce25baba36ffc19300f
l4szl0 <t0r0k.l4szl0@gmail.com>
2022-12-21 23:32:34 +0100

    add Rooms component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Offers from './components/Offers'
 import Plan from './components/Plan'
+import Rooms from './components/Rooms'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Offers />
       <Plan />
+      <Rooms />
     </div>
   );
 }
